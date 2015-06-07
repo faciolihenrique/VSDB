250d69131b201682e436274536c024e4b9f33f59
Henrique Noronha Facioli <henrique.facioli@students.ic.unicamp.br>
2015-06-07 15:02:30 -0300

    correcao path

---
+++ datas/teste.java
@@ -0,0 +1,15 @@
+public class teste {
+    private int idade;
+
+    public teste(){
+        this.idade = 0;
+    }
+
+    public void crescer(){
+        this.idade += 1;
+    }
+
+    public int getIdade(){
+        return this.idade;
+    }
+}
--- mainDb.java
+++ mainDb.java
@@ -1,11 +1,12 @@
 //Apenas para Teste
 
 public class mainDb{
+
     public static void main(String args[]){
         //Criacao da fabrica
         //Cada modulo deve colocar o nome do modulo (String) no construtor
         //Nesse caso seria o módulo "teste"
-        DBFactory factory = new DBFactory("file/teste");
+        DBFactory factory = new DBFactory("datas/teste");
 
         //Teste CSV
         //Retorna um vetor de string
@@ -13,18 +14,19 @@ public class mainDb{
         String[] csv = null;
         csv = objectDBCSV.getFromDB("Oi");
 
-        System.out.println("Toda a linha do CSV que possua a palavra Oi:\n ");
-        System.out.println(csv[0] + ", " + csv[1] + ", " + csv[2] + ", " + csv[3] + ", " + csv[4]);
+        System.out.println("Toda a linha do CSV que possua a palavra Oi:");
+        System.out.println(csv[0] + ", " + csv[1] + ", " + csv[2] + ", " + csv[3] + ", " + csv[4] + '\n');
 
         //Teste TXT
         //Retorna uma unica string que contem todo o conteudo do arquivo
         iDB objectDBTXT = factory.getDB(TypeDB.TXT);
         String txt = null;
         txt = objectDBTXT.getFromDB();
-        System.out.println("O arquivo em txt:\n");
+        System.out.println("O arquivo em txt:");
         System.out.println(txt);
 
         //Teste XML
         //Deve salvar qualquer tipo de objeto e de recuperá-lo!
+        iDB objectDBXML = factory.getDB(TypeDB.XML);
     }
 }
